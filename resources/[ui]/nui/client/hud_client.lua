c19d36e6e04d3d6a915d371a2780b8ed3c91abcf
Gautam Shetty <25051997gautampshetty@gmail.com>
2021-06-27 23:33:26 +0530

    Minimap update

--- resources/[ui]/nui/client/hud_client.lua
+++ resources/[ui]/nui/client/hud_client.lua
@@ -102,6 +102,16 @@ local h = 0.25
 Citizen.CreateThread(function()
 
     local minimap = RequestScaleformMovie("minimap")
+    RequestStreamedTextureDict("circlemap", false)
+    while not HasStreamedTextureDictLoaded("circlemap") do 
+        Wait(100)
+    end
+    AddReplaceTexture("platform:/textures/graphics", "radarmasksm", "circlemap", "radarmasksm")
+    SetMinimapClipType(1)
+    SetMinimapComponentPosition('minimap', 'L', 'B', x, y, w, h)
+    SetMinimapComponentPosition('minimap_mask', 'L', 'B', x + 0.17, y + 0.09, 0.072, 0.162)
+    SetMinimapComponentPosition('minimap_blur', 'L', 'B', -0.035, -0.03, 0.18, 0.22)
+    Wait(5000)
     SetRadarBigmapEnabled(true, false)
     Wait(0)
     SetRadarBigmapEnabled(false, false)
